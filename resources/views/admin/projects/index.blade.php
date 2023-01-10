cc9a9bf06d3e5ffbf0367ee52a983cb5d2f7c0cd
mattia.rollo <ste.tia@hotmail.it>
2023-01-10 16:37:01 +0100

    style index view with all projects cards

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,22 +4,26 @@
  <div class="container">
     <div class="row py-3">
         @foreach ($projects as $project)
-        <div class="col-6 g-2">
-            <div class="card text-bg-dark">
+        <div class="col-4 g-2">
+
+            <div class="card h-100">
+                <div class="card-body">
+                  <h5 class="card-title"><a href="{{ route('admin.projects.show', $project->slug) }}">{{$project->title}}</a></h5>
+                  <p class="card-text">{{$project->content}}</p>
+                  <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-primary">view</a>
+                </div>
+              </div>
+
+            {{-- <div class="card text-bg-dark">
                 <img src="https://via.placeholder.com/250x100.png?text=Visit+WhoIsHostingThis.com+Buyers+Guide" class="card-img" alt="...">
                 <div class="card-img-overlay">
                   <h5 class="card-title"><a href="{{ route('admin.projects.show', $project->slug) }}">{{$project->title}}</a></h5>
                   <p class="card-text">{{$project->content}}</p>
                   <p class="card-text"><small></small></p>
                 </div>
-            </div>
+            </div> --}}
         </div>
         @endforeach
     </div>
  </div>
- <ul>
-    @foreach ($projects as $project)
-        <li><a href="{{ route('admin.projects.show', $project->slug) }}">{{$project->title}}</a></li>
-    @endforeach
-</ul>
 @endsection
