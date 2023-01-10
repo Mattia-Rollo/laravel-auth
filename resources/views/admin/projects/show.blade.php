8a9be189feec077b472656c3e71fb0f78dac6c98
mattia.rollo <ste.tia@hotmail.it>
2023-01-10 16:39:30 +0100

    style single view project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container pt-2">
+
+    <h1>{{$project->title}}</h1>
+
+    <p>
+        {{$project->content}}
+    </p>
+    
+
+</div>
  
- <h1>{{$project->title}}</h1>
 @endsection
