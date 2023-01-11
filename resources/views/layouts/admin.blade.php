524857799f8d0543d13a6264fd75f12a76702c00
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2023-01-11 19:21:13 +0100

    style fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,24 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     
-<!--     Fonts and icons     -->
-{{-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" /> --}}
-
-<!-- Nucleo Icons -->
-{{-- <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
-<link href="./assets/css/nucleo-svg.css" rel="stylesheet" /> --}}
-
-<!-- Font Awesome Icons -->
-{{-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> --}}
-
-<!-- Material Icons -->
-{{-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet"> --}}
-
-<!-- CSS Files -->
-
-
-
-<link rel="stylesheet" href="/css/style.min.css">
+{{-- <link rel="stylesheet" href="/css/style.min.css"> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -44,7 +27,7 @@
      
     @include('partials.admin.sidebar')
 
-    <div id="main">
+    <div id="wrapper">
          
         <main class="container pt-2">
             @yield('content')
