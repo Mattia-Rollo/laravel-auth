68727c19db1b43c9e0f04a9e1b7e6ca9fcf04835
mattia.rollo <ste.tia@hotmail.it>
2023-01-11 15:46:26 +0100

    fix save path images in db table

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,7 +10,7 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'slug', 'content'];
+    protected $fillable = ['title', 'slug', 'content', 'cover_image'];
 
     public static function generateSlug($title)
     {
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -13,7 +13,7 @@
             {{$project->content}}
             {{$project->content}}
         </p>
-        <img src="{{asset('storage/' . $project->cover_image)}}" alt="" class="w-25">
+        <img class="w-25" src="{{asset('storage/' . $project->cover_image)}}" alt="" >
     <article>
 
         <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Back</a>
