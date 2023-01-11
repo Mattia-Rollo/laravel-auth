10678c5840382006eecdf5b111c3a6960a1f40c5
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2023-01-11 21:04:00 +0100

    style

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,14 +8,14 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
+                    @if (session('status')) 
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
                     <div class="d-flex justify-content-between">
                         <div class="pb-3">{{ __('You are logged in!') }}</div>
-                        <div class=""><a href={{ route('admin.projects.index') }}>Vedi Tutti i Progetti</a></div>
+                        <div class=""><a href={{ route('admin.projects.index') }}>Vedi Tutti i Progetti</a> <i class="fa-solid fa-house"></i></div>
                     </div>
                     <ul class="list-group">
                         @foreach ($projects as $project)
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -2,42 +2,6 @@
 
 @section('content')
 
-
-    edit
-
-    {{-- <article class="mb-5">
-        <h1 class="py-5">{{$project->title}}</h1>
-
-        <p>
-            {{$project->content}}
-            {{$project->content}}
-            {{$project->content}}
-            {{$project->content}}
-            {{$project->content}}
-        </p>
-    <article>
-
-        <form action="{{ route('admin.projects.update', $project->slug) }}" method="POST"
-            class="d-flex flex-column w-50 pb-3">
-            @csrf
-            @method('PUT')
-            
-            <div class="">
-                <label for="title" class="fs-3">Titolo</label>
-                <input type="text" name="title" id="title" class="form-control form-control-lg @error('title') is-invalid @enderror"
-                    value="{{ old('title', $project->title) }}">
-                @error('title')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-
-
-            <div class="py-2">
-                <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Back</a>
-                <button type="submit" value="Invia" class="btn btn-outline-primary">Salva</button>
-            </div>
-        </form> --}}
-
         <h1>Edit Post: {{$project->title}}</h1>
         <div class="row bg-white">
             <div class="col-12">
@@ -68,7 +32,7 @@
                         </div>
                     </div>
                       <button type="submit" class="btn btn-success">Submit</button>
-                      <button type="reset" class="btn btn-primary">Reset</button>
+                      <a href="{{route('admin.projects.index')}}" class="btn btn-primary">back</a>
                 </form>
             </div>
         </div>
